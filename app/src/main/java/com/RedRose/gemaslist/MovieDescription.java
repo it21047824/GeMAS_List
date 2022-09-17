1103df06610845c2e6dac5c58d59a99c2fcbae29
IT21085130 <it21085130@my.sliit.lk>
2022-09-17 23:40:17 +0530

    created movie list and description page

---
+++ app/src/main/java/com/RedRose/gemaslist/MovieDescription.java
@@ -0,0 +1,18 @@
+package com.RedRose.gemaslist;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MovieDescription extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_description, container, false);
+    }
+}
