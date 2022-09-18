d6a4c8664cbe47b6f3a98b44ea01f9d61a6e7b6d
it21047824 <it21047824@my.sliit.lk>
2022-09-18 12:10:50 +0530

    error commit

--- app/src/main/java/com/RedRose/gemaslist/SeriesList.java
+++ app/src/main/java/com/RedRose/gemaslist/SeriesList.java
@@ -3,10 +3,13 @@ package com.RedRose.gemaslist;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.button.MaterialButton;
 
 /**
  * A simple {@link Fragment} subclass.
