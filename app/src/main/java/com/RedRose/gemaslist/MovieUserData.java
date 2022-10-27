358aa3830f376507c38833ccbaaa4f092734f1b1
IT21085130 <it21085130@my.sliit.lk>
2022-10-27 12:59:12 +0530

    data Add,remove to firebase

---
+++ app/src/main/java/com/RedRose/gemaslist/MovieUserData.java
@@ -0,0 +1,127 @@
+package com.RedRose.gemaslist;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MovieUserData {
+    private static MovieUserData data;
+    private CustomLinkList watchingList;
+    private CustomLinkList planningList;
+    private CustomLinkList completedList;
+
+    private MovieUserData(){
+        watchingList = new CustomLinkList();
+        planningList = new CustomLinkList();
+        completedList = new CustomLinkList();
+    }
+
+    public static boolean dataInitialized(){
+        return data != null;
+    }
+
+    public static MovieUserData getMovieUserData() {
+        if(data == null) {
+            synchronized (MovieUserData.class){
+                data = new MovieUserData();
+            }
+        }
+        return data;
+    }
+
+    public AnimeDataEntry find(String title) {
+        AnimeDataEntry temp = watchingList.find(title);
+        if(temp == null) {
+            temp = planningList.find(title);
+            if(temp == null){
+                temp = completedList.find(title);
+            }
+        }
+        return temp;
+    }
+
+    public boolean remove(String title) {
+        boolean temp = false;
+        if(watchingList.size()>0){
+            temp = watchingList.removeItem(title);
+        }
+        if(!temp) {
+            if(planningList.size()>0){
+                temp = planningList.removeItem(title);
+            }
+        }
+        if(!temp){
+            if(completedList.size()>0){
+                temp = completedList.removeItem(title);
+            }
+        }
+        return temp;
+    }
+
+    public CustomLinkList getWatchingList() {
+        return watchingList;
+    }
+
+    public CustomLinkList getPlanningList() {
+        return planningList;
+    }
+
+    public CustomLinkList getCompletedList() {
+        return completedList;
+    }
+
+    public void setWatchingList(CustomLinkList watchingList) {
+        this.watchingList = watchingList;
+    }
+
+    public void setPlanningList(CustomLinkList planningList) {
+        this.planningList = planningList;
+    }
+
+    public void setCompletedList(CustomLinkList completedList) {
+        this.completedList = completedList;
+    }
+
+    public static boolean JSONStringToUserdata(String json) {
+        JSONObject retrievedData;
+        JSONArray dataArray;
+        MovieUserData userData = MovieUserData.getMovieUserData();
+        CustomLinkList watching = userData.getWatchingList();
+        CustomLinkList planning = userData.getPlanningList();
+        CustomLinkList completed = userData.getCompletedList();
+
+        try {
+            retrievedData = new JSONObject(json);
+            dataArray = retrievedData.getJSONArray("DATA");
+
+            for (int i = 0; i < dataArray.length(); i++) {
+                JSONObject obj = dataArray.getJSONObject(i);
+                String title = obj.getString("TITLE");
+                int status = obj.getInt("STATUS");
+                int rating = obj.getInt("RATING");
+                int progress = obj.getInt("PROGRESS");
+                boolean favourite = obj.getBoolean("FAVOURITE");
+
+                switch (status) {
+                    case FirebaseUtil.WATCHING:
+                        watching.addItem(new AnimeDataEntry(title, status, progress, rating, favourite));
+                        break;
+                    case FirebaseUtil.PLANNING:
+                        planning.addItem(new AnimeDataEntry(title, status, progress, rating, favourite));
+                        break;
+                    case FirebaseUtil.COMPLETED:
+                        completed.addItem(new AnimeDataEntry(title, status, progress, rating, favourite));
+                        break;
+                }
+            }
+            return true;
+        } catch (JSONException e) {
+            Log.e("Firebase", e.getMessage());
+        }
+        return false;
+
+    }
+
+}
